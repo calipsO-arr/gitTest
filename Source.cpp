067c52a6879d08e7f5e3c70ac50eae55edbce5a4
Max <Max@101-10>
2019-07-08 15:52:49 +0300

    Initial commit

---
+++ Source.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+
+void FillRand(int Arr[], const int n);
+void Print(int Arr[], const int n);
+
+
+void main() 
+{
+	setlocale(LC_ALL, "Russian");
+}
+
+void FillRand(int Arr[], const int n)
+{
+	for (int i = 0; i < n; i++)
+	{
+		Arr[i] = rand() % 100;
+	}
+}
+void Print(int Arr[], const int n)
+{
+	for (int i = 0; i < n; i++)
+	{
+		cout << Arr[i] << "\t";
+	}
+	cout << endl;
+}
